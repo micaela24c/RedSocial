0f72b5a7db1f097181a1e5249e550dd7d42e1bf5
Micaela <94004537+micaela24c@users.noreply.github.com>
2022-01-27 10:51:57 -0500

    9

--- assets/js/java.js
+++ assets/js/java.js
@@ -244,6 +244,10 @@ $(document).ready(function () {
           <div class="card bg-dark text-white  mt-3 mx-auto" style="max-width: 800px;">
             <div class="card-body">
               <p>${post.mensaje}</p>
+              
+              <div class="rateit" data-rateit-mode="font">
+              </div>
+              
               <img src="${post.urltext}" id="imagePost">
               <p>Publicado por ${post.userName}, el ${post.date}</p>
               <button data-id="${doc.id}" class="btn btn-success btn-edit-post bi bi-pencil">
@@ -263,6 +267,7 @@ $(document).ready(function () {
               
               <div class="rateit" data-rateit-mode="font">
               </div>
+
               <img src="${post.urltext}" id="imagePost">
               <p>Publicado por ${post.userName}, el ${post.date}</p>
             </div>
